5f70cf1da8f69080d68bddb40a82e37134f0c47a
Steve Baron <ndabasteve1@gmail.com>
2023-07-20 18:28:03 +0300

    Completed Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,65 @@
 import React from "react";
 import blogData from "../data/blog";
 
-console.log(blogData);
+
+
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+     <Header title={blogData.name} />
+     <About src = {blogData.image} about={blogData.about}  />
+     <ArticleList posts ={blogData.posts} />
     </div>
   );
-}
+  function Header (props){
+    return(
+      <h1>{props.title}</h1>
+   
+    )
+     }
+  function About(props){
+    return(
+      <aside>
+        <img src={props.image} placeholder ="https://via.placeholder.com/215Links to an external site." alt='blog logo' />
+        <p>{props.about} </p>
+      </aside>
+    )
+  }  
+  function  ArticleList(props){
+    let array = props.posts
+    
+    let articleArray = []
+    
+    array.forEach(x => {
+      articleArray.push(<Article key = {x.id} postTitle = {x.title} postDate = {x.date} postPreview={x.preview}/>
+      
+   ) })
+    
+     
 
+    
+
+    return(
+      <main>
+        {articleArray}
+        
+      </main>
+     )}
+      }
+    function Article(props){
+      return(
+      <article>
+        <h3>{props.postTitle}</h3>
+        <small>{props.postDate}</small>
+        <p>{props.postPreview}</p>
+      </article>
+      )
+      
+
+    }
+  
+
+
+  
 export default App;
